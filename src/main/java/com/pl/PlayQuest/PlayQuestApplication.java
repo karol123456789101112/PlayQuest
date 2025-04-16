860d1b9ae37d4739ed7fb0e9ff65fca4953ea880
Karol Kwapinski <karol.kwapinski1@interia.pl>
2025-04-16 12:35:09 +0200

    Project initialization

---
+++ src/main/java/com/pl/PlayQuest/PlayQuestApplication.java
@@ -0,0 +1,13 @@
+package com.pl.PlayQuest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class PlayQuestApplication extends SpringBootServletInitializer {
+	public static void main(String[] args) {
+		SpringApplication.run(PlayQuestApplication.class, args);
+	}
+}
+
